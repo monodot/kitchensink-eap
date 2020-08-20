ff0914ee2cda13cc444479db328284c1a7e69209
Tom Donohue <monodot@gmail.com>
2020-08-20 12:57:08 +0100

    wip

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 echo "Executing patch.cli"
-$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/cli-example.cli
+echo "HELLO from my script! I could do some extra configuration here....."
+echo "..........."
+echo "..........."
+
+# $JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/cli-example.cli
